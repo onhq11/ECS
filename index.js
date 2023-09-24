57db71e2504c8aa9abb7a554c1164c50e9abc27b
onhq11 <91978377+onhq11@users.noreply.github.com>
2023-09-24 21:02:28 +0200

    Release v1.0 (#2)

--- index.js
+++ index.js
@@ -1,22 +1,160 @@
+const fs = require("fs")
 const http = require('http')
 const { exec } = require('child_process')
+let customScripts
 
-const port = 41420
-const customScripts = [
-    {trigger: "/tv_on", path: "/home/onhq/tvScripts/tv_on.sh"},
-    {trigger: "/tv_off", path: "/home/onhq/tvScripts/tv_off.sh"}
-]
-const startupCommands = [
-    `sudo echo 20 > /sys/class/gpio/export`,
-    `sudo echo "out" > /sys/class/gpio/gpio20/direction`,
-    `sudo echo 21 > /sys/class/gpio/export`,
-    `sudo echo "out" > /sys/class/gpio/gpio21/direction`
-]
+const init = () => {
+    const config = readConfig()
 
-const initialStartup = () => {
-    startupCommands.forEach((command) => {
-        execCommand(command)
+    if(config.length > 0) {
+        startServer(JSON.parse(config))
+    }
+}
+
+const readConfig = () => {
+    return fs.readFileSync("./config.json", "utf-8")
+}
+
+const startServer = (config) => {
+    if(!config.port) {
+        console.log(`\n\n\n----------------- CHECK CONFIG (PORT IS INVALID) -----------------\n`)
+        return
+    }
+
+    const server = http.createServer(requestListener)
+    server.listen(config.port, () => {
+        console.log(`\n\n\n----------------- SERVER IS RUNNING ON ${config.port} -----------------\n`)
+
+        if(config.startupCommands?.length > 0) {
+            config.startupCommands.forEach((command) => {
+                execCommand(command)
+            })
+        }
+
+        if(config.customScripts?.length > 0) {
+            customScripts = config.customScripts
+        }
     })
+}
+
+const requestListener = (req, res) => {
+    if(req.url === '/favicon.ico') return
+    let error = false
+
+    console.log(`\n----------------- ${req.url} -----------------\n`)
+
+    let webUI = false
+    switch (req.url) {
+        case "/": {
+            fs.readFile('./ui/webui.html', (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(`{"status": "error", "details": "failed to open webui"}`);
+                } else {
+                    let htmlContent = data.toString()
+                    if(customScripts?.length > 0) {
+                        htmlContent = htmlContent.replace('"{{customScripts}}"', JSON.stringify(customScripts));
+                    }
+
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(htmlContent);
+                }
+            });
+            webUI = true
+        }
+
+        case "/light-theme.css":
+        case "/dark-theme.css":
+        case "/global.css": {
+            fs.readFile(`./ui${req.url}`, (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(`{"status": "error", "details": "failed to open webui"}`);
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/css' });
+                    res.end(data.toString());
+                }
+            });
+            webUI = true
+        }
+
+        case "/iconify/sun.svg":
+        case "/iconify/moon.svg": {
+            fs.readFile(`./ui${req.url}`, (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(`{"status": "error", "details": "failed to open webui"}`);
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+                    res.end(data.toString());
+                }
+            });
+            webUI = true
+        }
+    }
+    if(webUI) return
+
+    const requestBody = req.url.split("/")
+    const type = requestBody[1]
+
+    if(type === "gpio") {
+        const pin = requestBody[2]
+        const action = requestBody[3]
+
+        switch(action) {
+            case "export":
+                execCommand(`sudo echo ${pin} > /sys/class/gpio/export`)
+                break
+
+            case "unexport":
+                execCommand(`sudo echo ${pin} > /sys/class/gpio/unexport`)
+                break
+
+            case "in":
+                execCommand(`sudo echo "in" > /sys/class/gpio/gpio${pin}/direction`)
+                break
+
+            case "out":
+                execCommand(`sudo echo "out" > /sys/class/gpio/gpio${pin}/direction`)
+                break
+
+            case "1":
+            case "0":
+                execCommand(`sudo echo ${action} > /sys/class/gpio/gpio${pin}/value`)
+                break
+
+            default: {
+                throwErr(res, `GPIO ACTION IS INVALID ${req.url}`, "gpio action is invalid")
+                error = true
+            }
+        }
+    } else {
+        if(!customScripts?.length > 0) {
+            throwErr(res, `ACTION NOT RECOGNIZED ${req.url}`, "action not recognized")
+            return
+        }
+
+        const action = customScripts.find((item) => item.trigger === req.url)
+
+        if(!action?.toString()?.length > 0) {
+            throwErr(res, `ACTION NOT RECOGNIZED ${req.url}`, "action not recognized")
+            return
+        }
+
+        execCommand(`bash ${action.path}`)
+    }
+
+    if(error) return
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end('{"status": "ok"}')
+}
+
+const throwErr = (res, text, details) => {
+    console.log(`----------------- ${text} -----------------\n`)
+    error = true
+
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(`{"status": "error", "details": "${details}"}`)
 }
 
 const execCommand = async (command) => {
@@ -36,48 +174,4 @@ const execCommand = async (command) => {
     })
 }
 
-const requestListener = (req, res) => {
-    if(req.url === '/favicon.ico') return
-
-    console.log(`\n\n----------------- ${req.url} -----------------\n`)
-
-    const requestType = req.url.split(/[0-9]+/)[0];
-    const pin = req.url.match(/\d+/);
-    const state = req.url.match(/-(\d+)/)?.[1];
-
-    switch(requestType) {
-        case "/export":
-            execCommand(`sudo echo ${pin} > /sys/class/gpio/export`)
-            break
-
-        case "/unexport":
-            execCommand(`sudo echo ${pin} > /sys/class/gpio/unexport`)
-            break
-
-        case "/in":
-            execCommand(`sudo echo "in" > /sys/class/gpio/gpio${pin}/direction`)
-            break
-
-        case "/out":
-            execCommand(`sudo echo "out" > /sys/class/gpio/gpio${pin}/direction`)
-            break
-
-        case "/gpio":
-            execCommand(`sudo echo ${state} > /sys/class/gpio/gpio${pin}/value`)
-            break
-
-        default: {
-            const requestedScript = customScripts.find(item => item.trigger === req.url)
-            execCommand("bash "+requestedScript.path)
-        }
-    }
-
-    res.writeHead(200)
-    res.end('OK')
-}
-
-const server = http.createServer(requestListener)
-server.listen(port, () => {
-    console.log(`\n\n\n----------------- SERVER IS RUNNING ON ${port} -----------------\n`)
-    initialStartup()
-})
+init()
